03736be19fc0889afa7c8ecd2e8ae2a68e25d126
sheikoodally <44279883+sheikoodally@users.noreply.github.com>
2020-06-28 13:00:51 -0400

    adding api and axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [advice, setAdvice] = useState(
+    {  
+      id:'4',
+      advise:'he', 
+    });
+  console.log(advice);
+
+  const url = 'https://api.adviceslip.com/advice';
+
+  useEffect(() => {
+    //fetchAdvice();
+  },[]);
+
+  const fetchAdvice = async () => {
+    try{
+
+      const response = await axios.get(url);
+      const updatedResponse = response.data.slip;
+      await axios.get('https://api.adviceslip.com/advice')
+      console.log(updatedResponse);
+      setAdvice([advice.id == updatedResponse.id, advice.advise == updatedResponse.advice]);
+    }
+      catch(error) {
+        console.log(error + " error");
+      };
+  }
+
+
+  const test = () => {
+    setAdvice ([advice.id = 100, advice.advise = 'heyyyy']);
+  }
+  
+//<h1 className="heading">{advice}</h1>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+        <div className="card">
+        <h1 className="heading">{advice.advise}</h1>
+          <button className="button" onClick={test}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
   );
 }
 
